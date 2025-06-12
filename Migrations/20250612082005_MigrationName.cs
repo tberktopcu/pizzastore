38c457e0ac56bbafcf49cbc8768038271b0cfc84
tahir <tahirberktopcu@gmail.com>
2025-06-12 08:51:43 +0000

    İlk commit

---
+++ Migrations/20250612082005_MigrationName.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzaStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "test",
+                table: "Pizzas",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Pizzas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "test",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "test",
+                table: "Pizzas");
+        }
+    }
+}
